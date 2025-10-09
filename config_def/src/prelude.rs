0fb56cf3eb11036501b2beadfe4763d762d60496
volodymyrd <volodymyrdotsenko@gmail.com>
2025-10-09 18:48:08 +0200

    update prelude.

--- config_def/src/prelude.rs
+++ config_def/src/prelude.rs
@@ -1,7 +1,9 @@
 //! The `easy_config_def` prelude.
 
 pub use crate::errors::ConfigError;
+pub use crate::types::password::Password;
 pub use crate::validators::{
     Validator, range::Range, valid_list::ValidList, valid_string::ValidString,
 };
+pub use crate::{ConfigDef, ConfigKey, ConfigValue, FromConfigDef, Importance};
 pub use easy_config_macros::EasyConfig;
